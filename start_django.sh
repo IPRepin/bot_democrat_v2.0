0d2e1aa67f5f9b1fe8ee64abe7bbf7bfd2cdcdf5
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2023-10-18 09:21:38 +0300

    fix docker-compose

--- start_django.sh
+++ start_django.sh
@@ -4,12 +4,12 @@
 #python manage.py flush --no-input
 
 # migration to application
-python3 app_django.py makemigrations
-python3 app_django.py migrate
+python3 django_app.py makemigrations
+python3 django_app.py migrate
 # collect static files
-python3 app_django.py collectstatic --no-input --clear
+python3 django_app.py collectstatic --no-input --clear
 
 # create superuser for postgres
 # python manage.py createsuperuser --email=admin@admin.com --noinput
 
-python3 app_django.py runserver 0.0.0.0:8000
+python3 django_app.py runserver 0.0.0.0:8000
